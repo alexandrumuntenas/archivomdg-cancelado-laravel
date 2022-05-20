0885db5b47653943f651f49af87a8a2567420f02
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-20 18:42:40 +0200

    Finalizar eventosControler.

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -11,4 +11,37 @@ class EventosController extends Controller
         $eventos = Evento::all();
         return response()->json($eventos);
     }
+    public function obtenerEvento($id) {
+        $evento = Evento::find($id);
+        return response()->json($evento);
+    }
+    public function crearEvento (Request $request) {
+        $evento = new Evento();
+        $evento->nombre = $request->nombre;
+        $evento->descripcion = $request->descripcion;
+        $evento->fecha = $request->fecha;
+        $evento->save();
+        return response()->json($evento);
+    }
+    public function editarEvento (Request $request) {
+        $evento = Evento::find($request->id);
+        $evento->nombre = $request->nombre;
+        $evento->descripcion = $request->descripcion;
+        $evento->fecha = $request->fecha;
+        $evento->save();
+        return response()->json($evento);
+    }
+    public function eliminarEvento (Request $request) {
+        $evento = Evento::find($request->id);
+        $evento->delete();
+        return response()->json($evento);
+    }
+    public function verEvento (Request $request) {
+        if ($request->id) {
+            $evento = Evento::find($request->id);
+            return view('eventos.verEvento', ['evento' => $evento]);
+        } else {
+            return redirect('/dashboard');
+        }
+    }
 }
