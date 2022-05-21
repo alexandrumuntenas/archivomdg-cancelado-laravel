8fef0991d401f947f50e786bc2bf2d57a61ef87a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-21 18:05:35 +0200

    Crear CRUD para PartiturasController

--- app/Http/Controllers/PartiturasController.php
+++ app/Http/Controllers/PartiturasController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Partitura;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PartiturasController extends Controller
 {
-    //
+    public function subirPartitura (Request $request)
+    {
+        $partitura = new Partitura();
+        $partitura->nombre = $request->nombre;
+        $partitura->archivo = $request->file('archivo')->storeAs('partituras', $request->file('archivo')->getClientOriginalName());
+        $partitura->save();
+        return $partitura;
+    }
+    public function editarPartitura (Request $request)
+    {
+        $partitura = Partitura::find($request->id);
+        $partitura->nombre = $request->nombre;
+        $partitura->archivo = $request->file('archivo')->storeAs('partituras', $request->file('archivo')->getClientOriginalName());
+        $partitura->save();
+        return $partitura;
+    }
+    public function eliminarPartitura (Request $request)
+    {
+        $partitura = Partitura::find($request->id);
+        $partitura->delete();
+        return $partitura;
+    }
 }
