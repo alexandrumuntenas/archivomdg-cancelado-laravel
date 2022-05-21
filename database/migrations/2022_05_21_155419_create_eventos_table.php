d1a6fcaadf9c0cb8abfbac1fe88017a5e73417a5
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-21 17:57:18 +0200

    Crear migraciones fundamentales.

--- database/migrations/2022_05_21_155419_create_eventos_table.php
+++ database/migrations/2022_05_21_155419_create_eventos_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('eventos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('cuerda');
-            $table->rememberToken();
+            $table->string('nombre')->unique();
+            $table->date('fecha');
+            $table->time('hora');
+            $table->string('lugar');
+            $table->string('participantes');
+            $table->string('partituras');
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('eventos');
     }
 };
