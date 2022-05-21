89aa6c7acaa772ccb671e0bf8e20ed134532e6d1
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-21 18:10:53 +0200

    Crear función obtenerPartituras en función de cuerda.

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cuerda;
 use App\Models\Evento;
+use App\Models\Partitura;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EventosController extends Controller
 {
-    public function crearEvento (Request $request)
+    public function crearEvento(Request $request)
     {
         $evento = new Evento();
         $evento->nombre = $request->nombre;
@@ -19,7 +22,7 @@ class EventosController extends Controller
         $evento->save();
         return $evento;
     }
-    public function editarEvento (Request $request)
+    public function editarEvento(Request $request)
     {
         $evento = Evento::find($request->id);
         $evento->nombre = $request->nombre;
@@ -31,10 +34,27 @@ class EventosController extends Controller
         $evento->save();
         return $evento;
     }
-    public function eliminarEvento (Request $request)
+    public function eliminarEvento(Request $request)
     {
         $evento = Evento::find($request->id);
         $evento->delete();
         return $evento;
+    }
+    public function obtenerPartituras(Request $request)
+    {
+        $partituras = explode(',', Evento::find($request->id)->partituras)->map(function ($partitura) {
+            return Partitura::find($partitura);
+        });
+
+        // filtrar partituras por cuerda
+        $partituras = array_filter($partituras, function ($value) {
+            if (str_starts_with(explode('_', $value)[1], strtolower(Cuerda::all()->find(Auth::user()->cuerda)->nombre))) {
+                return $value;
+            } else {
+                return false;
+            }
+        });
+
+        return $partituras;
     }
 }
