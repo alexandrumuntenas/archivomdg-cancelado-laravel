4de82f5fbcae14925ea0bc3f06a64fb9c5b33d33
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-21 18:01:41 +0200

    Crear funciones CRUD de CuerdasController

--- app/Http/Controllers/CuerdasController.php
+++ app/Http/Controllers/CuerdasController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cuerda;
 use Illuminate\Http\Request;
 
 class CuerdasController extends Controller
 {
-    //
+    public function crearCuerda (Request $request)
+    {
+        $cuerda = new Cuerda();
+        $cuerda->nombre = $request->nombre;
+        $cuerda->save();
+        return $cuerda;
+    }
+    public function editarCuerda (Request $request)
+    {
+        $cuerda = Cuerda::find($request->id);
+        $cuerda->nombre = $request->nombre;
+        $cuerda->save();
+        return $cuerda;
+    }
+    public function eliminarCuerda (Request $request)
+    {
+        $cuerda = Cuerda::find($request->id);
+        $cuerda->delete();
+        return $cuerda;
+    }
 }
