b57dfae08b609d595bd949e71ac6bc834622c661
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-21 18:02:49 +0200

    Crear funciones CRUD de EventosController

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evento;
 use Illuminate\Http\Request;
 
 class EventosController extends Controller
 {
-    //
+    public function crearEvento (Request $request)
+    {
+        $evento = new Evento();
+        $evento->nombre = $request->nombre;
+        $evento->fecha = $request->fecha;
+        $evento->hora = $request->hora;
+        $evento->lugar = $request->lugar;
+        $evento->participantes = $request->participantes;
+        $evento->partituras = $request->partituras;
+        $evento->save();
+        return $evento;
+    }
+    public function editarEvento (Request $request)
+    {
+        $evento = Evento::find($request->id);
+        $evento->nombre = $request->nombre;
+        $evento->fecha = $request->fecha;
+        $evento->hora = $request->hora;
+        $evento->lugar = $request->lugar;
+        $evento->participantes = $request->participantes;
+        $evento->partituras = $request->partituras;
+        $evento->save();
+        return $evento;
+    }
+    public function eliminarEvento (Request $request)
+    {
+        $evento = Evento::find($request->id);
+        $evento->delete();
+        return $evento;
+    }
 }
