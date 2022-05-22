99614ce05b4fc926c211d897d6ec867484358bfd
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-22 12:56:11 +0200

    Añadir rutas anteriores al reseteo.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EventosController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,24 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(EventosController::class)->group(function () {
+    Route::get('/eventos', 'obtenerEventos')->name('obtenerEventos')->middleware(['auth']);
+    Route::get('/eventos/participa', 'obtenerEventosDelUsuario')->name('obtenerEventosDelUsuario')->middleware(['auth']);
+    Route::get('/eventos/evento/{id?}', 'verEvento')->name('verEvento')->middleware(['auth']);
+    Route::get('/eventos/ver', 'verEventos')->name('verEventos')->middleware(['auth']);
+    Route::post('/eventos/crear', 'crearEvento')->name('crearEvento')->middleware(['auth']);
+    Route::post('/eventos/editar', 'editarEvento')->name('editarEvento')->middleware(['auth']);
+    Route::post('/eventos/eliminar', 'eliminarEvento')->name('eliminarEvento')->middleware(['auth']);
+    Route::get('/eventos/archivos/{id}', 'obtenerArchivos')->name('archivos')->middleware(['auth']);
+});
+
+/*
+Route::controller(FileManagerController::class)->group(function () {
+    Route::get('/archivos/{path?}', 'index')->name('filemanager')->middleware(['auth']);
+    Route::post('/archivos/subir', 'subirArchivo')->name('subirArchivo')->middleware(['auth']);
+    Route::post('/archivos/eliminar', 'eliminarArchivo')->name('eliminarArchivo')->middleware(['auth']);
+    Route::post('/archivos/renombrar', 'renombrarArchivo')->name('renombrarArchivo')->middleware(['auth']);
+    Route::get('/archivos/descargar/{id?}/{path?}', 'descargarArchivo')->name('descargarArchivo')->middleware(['auth']);
+});
+*/
