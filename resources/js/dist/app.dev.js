aeb6c306f104ff88f205fb9b335c3c1c012c5dd1
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-22 15:16:53 +0200

    Cosas de tailwind.

---
+++ resources/js/dist/app.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _alpinejs = _interopRequireDefault(require("alpinejs"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+require('./bootstrap');
+
+window.Alpine = _alpinejs["default"];
+
+_alpinejs["default"].start();
