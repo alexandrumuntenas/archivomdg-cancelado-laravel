c8cd6858ac5d03cfca6c789e99e5c89907451575
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-22 15:16:41 +0200

    Actualizar Rutas.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\EventosController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventosController;
+use App\Http\Controllers\PartiturasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,27 +19,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return redirect('/miseventos');
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
 
 Route::controller(EventosController::class)->group(function () {
-    Route::get('/eventos', 'obtenerEventos')->name('obtenerEventos')->middleware(['auth']);
-    Route::get('/eventos/participa', 'obtenerEventosDelUsuario')->name('obtenerEventosDelUsuario')->middleware(['auth']);
-    Route::get('/eventos/evento/{id?}', 'verEvento')->name('verEvento')->middleware(['auth']);
+    Route::get('/eventos/miseventos', 'index')->name('misEventos')->middleware(['auth']);
     Route::get('/eventos/ver', 'verEventos')->name('verEventos')->middleware(['auth']);
+    Route::get('/eventos/evento/{id?}', 'verEvento')->name('verEvento')->middleware(['auth']);
+
+    // API
+    Route::get('/eventos/detalles/{id?}', 'obtenerEvento')->name('obtenerEvento')->middleware(['auth']);
+    Route::get('/eventos/detalles/{id?}/partituras', 'obtenerPartituras')->name('obtenerPartituras')->middleware(['auth']);
+    Route::get('/eventos/todos', 'obtenerEventos')->name('obtenerEventos')->middleware(['auth']);
+    Route::get('/eventos/participa', 'obtenerEventosDelUsuario')->name('obtenerEventosDelUsuario')->middleware(['auth']);
     Route::post('/eventos/crear', 'crearEvento')->name('crearEvento')->middleware(['auth']);
     Route::post('/eventos/editar', 'editarEvento')->name('editarEvento')->middleware(['auth']);
     Route::post('/eventos/eliminar', 'eliminarEvento')->name('eliminarEvento')->middleware(['auth']);
-    Route::get('/eventos/archivos/{id}', 'obtenerArchivos')->name('archivos')->middleware(['auth']);
 });
 
-/*
-Route::controller(FileManagerController::class)->group(function () {
-    Route::get('/archivos/{path?}', 'index')->name('filemanager')->middleware(['auth']);
-    Route::post('/archivos/subir', 'subirArchivo')->name('subirArchivo')->middleware(['auth']);
-    Route::post('/archivos/eliminar', 'eliminarArchivo')->name('eliminarArchivo')->middleware(['auth']);
-    Route::post('/archivos/renombrar', 'renombrarArchivo')->name('renombrarArchivo')->middleware(['auth']);
-    Route::get('/archivos/descargar/{id?}/{path?}', 'descargarArchivo')->name('descargarArchivo')->middleware(['auth']);
+Route::controller(PartiturasController::class)->group(function () {
+    Route::get('/partituras/{path?}', 'index')->name('filemanager')->middleware(['auth']);
+    Route::post('/partituras/subir', 'subirArchivo')->name('subirArchivo')->middleware(['auth']);
+    Route::post('/partituras/eliminar', 'eliminarArchivo')->name('eliminarArchivo')->middleware(['auth']);
+    Route::post('/partituras/renombrar', 'renombrarArchivo')->name('renombrarArchivo')->middleware(['auth']);
+    Route::get('/partituras/descargar/{partitura?}/{cuerda?}', 'descargarPartitura')->name('descargarPartitura')->middleware(['auth']);
 });
-*/
