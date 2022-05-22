2f607e5d6dd6b383e0ad06ebc27a742835e449b3
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-22 16:24:50 +0200

    Comenzar a trabajar en el modal de creación de eventos.

--- resources/js/dist/app.dev.js
+++ resources/js/dist/app.dev.js
@@ -2,6 +2,8 @@
 
 var _alpinejs = _interopRequireDefault(require("alpinejs"));
 
+require("tw-elements");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 require('./bootstrap');
